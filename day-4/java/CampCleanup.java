ec0fbca26e1ab4496c38e36fafebf80c81c61080
Anton Fedoruk <antobn.fedoruk@gmail.com>
2023-05-01 17:22:15 +0300

    DAY-4: Camp Cleanup task.

---
+++ day-4/java/CampCleanup.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CampCleanup {
+    public static void main(String[] args) {
+        String path = "day-4/resources/input.txt";
+
+        System.out.println("howManyAssignmentPairsDoesOneRangeFullyContainTheOther: " + howManyAssignmentPairsDoesOneRangeFullyContainTheOther(path));
+        // 459
+
+        System.out.println("howManyAssignmentPairsOverlapEachOther: " + howManyAssignmentPairsOverlapEachOther(path));
+        // 779
+    }
+
+    public static int howManyAssignmentPairsDoesOneRangeFullyContainTheOther(String fileName) {
+        int oneRangeFullyContainTheOtherCount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String pair = bufferedReader.readLine();
+            while (pair != null) {
+                String[] ranges = pair.split(",");
+                Range r1 = new Range(ranges[0]);
+                Range r2 = new Range(ranges[1]);
+                if (r1.contain(r2) | r2.contain(r1)) {
+                    oneRangeFullyContainTheOtherCount++;
+                }
+                pair = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return oneRangeFullyContainTheOtherCount;
+    }
+
+    public static int howManyAssignmentPairsOverlapEachOther(String fileName) {
+        int overlapPairCount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String pair = bufferedReader.readLine();
+            while (pair != null) {
+                String[] ranges = pair.split(",");
+                Range r1 = new Range(ranges[0]);
+                Range r2 = new Range(ranges[1]);
+                if (r1.overlap(r2) | r2.overlap(r1)) {
+                    overlapPairCount++;
+                }
+                pair = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return overlapPairCount;
+    }
+}
+
+class Range {
+    int start;
+    int end;
+
+    boolean is1Range;
+
+    public Range(String rangeWithHyphen) {
+        start = Integer.parseInt(rangeWithHyphen.split("-")[0]);
+        end = Integer.parseInt(rangeWithHyphen.split("-")[1]);
+        is1Range = start == end;
+    }
+
+    boolean contain(Range other) {
+        if (is1Range) {
+            if (other.start <= start && start <= other.end) {
+                return true;
+            }
+        }
+
+        if (start <= other.start) {
+            if (other.end <= end) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean overlap(Range other) {
+        if (contain(other)) return true;
+        return start <= other.start && other.start <= end;
+    }
+}
